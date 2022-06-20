a27f5809b1fd895c784c9a468fb0571f425a4cdb
Gabriel342 <gabrielsev_2002@hotmail.com>
2022-06-20 00:43:19 -0300

    register

--- src/app/auth/registrar/registrar.component.ts
+++ src/app/auth/registrar/registrar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      senha: this.fb.control('', [Validators.required]),
+      nome: this.fb.control('', [Validators.required]),
+      idade: this.fb.control('', [Validators.required])
+    });
+  }
+
+  registrar() { }
 
   ngOnInit(): void {
+
   }
 
 }
